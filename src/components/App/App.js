60b973dda4a0ae5973a8c5be8ac3e8eea79afb67
SNx42 <seannowotny@live.de>
2019-11-23 11:46:24 +0900

    Test TodoStore and enable decorators

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import TodoStore from '../../stores/TodoStore/TodoStore';
 
-function App() {
+function App() 
+{
+  useEffect(() =>
+  {
+    TodoStore.addTodo("read MobX tutorial");
+    TodoStore.addTodo("try MobX");
+    TodoStore.todos[0].completed = true;
+    TodoStore.todos[1].task = "try MobX in own project";
+    TodoStore.todos[0].task = "grok MobX tutorial";
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello</h1>
